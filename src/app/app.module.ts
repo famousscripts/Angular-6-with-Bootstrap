79f36fc0ef2d34c43f8d5d96b29cfe6d1707b3db
Ghanshyam pandey <44994843+famousscript@users.noreply.github.com>
2018-11-27 20:46:07 +0530

    Bootstrap in angular 6
    Bootstrap in angular 6

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import {AppRoutingModule} from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
